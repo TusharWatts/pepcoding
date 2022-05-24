6093f2b866b2b339977a6a9156274ee65160b775
TusharWatts <tushtiit@gmail.com>
2022-05-25 04:25:14 +0530

    Update next-greater-element.java

--- stack-&-queue/next-greater-element.java
+++ stack-&-queue/next-greater-element.java
@@ -2,14 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-  public static void display(int[] a){
-    StringBuilder sb = new StringBuilder();
-
-    for(int val: a){
-      sb.append(val + "\n");
-    }
-    System.out.println(sb);
-  }
+  
 
 public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,34 +12,96 @@ public static void main(String[] args) throws Exception {
     for(int i = 0; i < n; i++){
        a[i] = Integer.parseInt(br.readLine());
     }
+    int k = Integer.parseInt(br.readLine());
 
-    int[] nge = solve(a);
-    display(nge);
+    int[] result = calculateMaxSlidingWindow(a, k);
+    
+    for(int elem: result){
+        System.out.println(elem);
+    }
+    
+}
+ 
+ /*
+ * O(n^2) approach (brute force)
+ */
+//  public static int[] calculateMaxSlidingWindow(int[] arr, int window_size){
+     
+//      int[] result = new int[arr.length-window_size+1];
+     
+//      for(int outer = 0; outer <= arr.length-window_size; outer++){
+         
+//          int max = arr[outer];
+//          for(int inner = outer+1; inner < outer+window_size; inner++){
+//              max = Math.max(max, arr[inner]);
+//          }
+         
+//          result[outer] = max;
+         
+//      }
+     
+//      return result;
+     
+     
+//  }
+ 
+ /*
+ * Find next greater element to the right of each elements of the array
+ */
+ public static int[] getNgeR(int[] arr){
+     
+     int[] ngeR = new int[arr.length];
+     Stack<Integer> stk = new Stack<>();
+     
+     for(int i = arr.length-1; i >= 0; i--){
+         
+         int val = arr[i];
+         
+         while(stk.isEmpty() == false && arr[stk.peek()] < val){
+             stk.pop();
+         }
+         
+         ngeR[i] = stk.isEmpty() ? -1 : stk.peek();
+         stk.push(i);
+         
+     }
+     
+     return ngeR; 
+     
  }
-
- public static int[] solve(int[] arr){
-   
-   int arrLen = arr.length;
-   int[] nge = new int[arrLen];
-   
-   Stack<Integer> stk = new Stack<>();
-   
-   for(int i = arrLen-1; i >= 0; i--){
-       
-        int currVal = arr[i];
-       
-        //Pop smaller elements
-        while(stk.size() > 0 && stk.peek() < currVal){
-            stk.pop();
-        }
-        
-        //Answer
-        nge[i] = stk.isEmpty() ? -1 : stk.peek();
-        
-        stk.push(currVal);
-       
-   }
-   
-   return nge;
-   
+ 
+ 
+ /*
+ * O(n) approach using stacks
+ */
+ public static int[] calculateMaxSlidingWindow(int[] arr, int window_size){
+     
+     int[] ngeR = getNgeR(arr);
+     
+     int[] result = new int[arr.length - window_size+1];
+     
+     int prevWinMaxIdx = 0;
+     for(int outer = 0; outer < arr.length - window_size+1; outer++){
+         
+         int currIdx = prevWinMaxIdx;
+         
+         //If prev window max doesn't lie in the current window, start from the first element of the window
+         if(currIdx < outer) currIdx = outer;
+         
+         //Jump to n.g.e (right) till it lies in the current window
+         int ngeRIdx = ngeR[currIdx];
+         while( ngeRIdx > outer && ngeRIdx < outer + window_size ){
+             currIdx = ngeRIdx;             
+             ngeRIdx = ngeR[ngeRIdx];
+         }
+         
+         prevWinMaxIdx = currIdx;
+         result[outer] = arr[currIdx];
+         
+     }
+     
+     return result;
+     
  }
+ 
+}
